9b58965f6108f5ebae2c94fe3c121a48d067daa4
bryandms <bryandms95@gmail.com>
2019-02-15 15:28:20 -0600

    Add dialectOptions and host

--- models/index.js
+++ models/index.js
@@ -3,12 +3,17 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
+const host = process.env.DB_HOST
 const database = process.env.DB_DATABASE
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
 
 const sequelize = new Sequelize(database, username, password, {
-  dialect: 'postgres'
+  host,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
 })
 
 const db = {
